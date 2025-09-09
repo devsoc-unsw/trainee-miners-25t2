f9b7b0734c1e469b028636b112241fd629bbbd71
Zayan Farazi <zayan.farazi1@gmail.com>
2025-09-09 22:18:19 +1000

    feat: started adding server routing for google oauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,25 @@
-import { handlers } from "~/server/auth";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "~/server/db";
 
-export const { GET, POST } = handlers;
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  // session: { strategy: "database" }, // or "jwt" (pick one that fits your app)
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
+  callbacks: {
+    session: ({ session, user, token }) => {
+      // add user.id to session.user for convenience
+      if (session.user) {
+        // user is only guaranteed during some flows; fall back to token.sub
+        (session.user as any).id = user?.id ?? token?.sub ?? null;
+      }
+      return session;
+    },
+  },
+});
